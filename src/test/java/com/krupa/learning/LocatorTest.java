a9347177df300f0c8f239caa11c3b29f5a082375
QAJani <krupa0602@gmail.com>
2026-01-11 14:46:18 -0500

    Click A/B Testing link and validate destination page

--- src/test/java/com/krupa/learning/LocatorTest.java
+++ src/test/java/com/krupa/learning/LocatorTest.java
@@ -21,15 +21,18 @@ public class LocatorTest {
         Assert.assertEquals(heading, "Welcome to the-internet",
                 "Heading text did not match");
         // Locator by linkText
-        String linkText = driver.findElement(By.linkText("A/B Testing")).getText();
-        Assert.assertEquals(linkText, "A/B Testing");
-        //Locator by xPath
+        // String linkText = driver.findElement(By.linkText("A/B Testing")).getText();
+        // Assert.assertEquals(linkText, "A/B Testing");
+        // Locator by xPath
         String subheading = driver.findElement(By.xpath("//h2")).getText();
         Assert.assertEquals(subheading, "Available Examples");
         // Locator by CSSSelector
         String abLinkCss = driver.findElement(By.cssSelector("a[href='/abtest']")).getText();
         Assert.assertEquals(abLinkCss, "A/B Testing", "A/B Testing link text did not match");
-
+        // Actions
+        driver.findElement(By.cssSelector("a[href='/abtest']")).click();
+        String title = driver.findElement(By.cssSelector("h3")).getText();
+        Assert.assertEquals(title, "A/B Test Control", "Incorrect heading match");
         driver.quit();
     }
 }
