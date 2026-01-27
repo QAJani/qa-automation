80d3d7400ca82ca02a6edc7970015bfe37b6a1ee
QAJani <krupa0602@gmail.com>
2026-01-27 17:16:18 -0500

    Add BaseTest with setup and teardown (Chapter 5)

--- src/test/java/com/krupa/learning/selenium/LoginTest.java
+++ src/test/java/com/krupa/learning/selenium/LoginTest.java
@@ -1,45 +1,28 @@
 package com.krupa.learning.selenium;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
 
     @Test
     public void LoginWithValidCredentials() {
 
-        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
 
-        try {
-            driver.manage().window().maximize();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
 
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.urlContains("/secure"));
 
-            driver.get("https://the-internet.herokuapp.com/login");
+        String flashText = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")))
+                .getText();
 
-            driver.findElement(By.id("username")).sendKeys("tomsmith");
-            driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-            driver.findElement(By.cssSelector("button[type='submit']")).click();
-
-            wait.until(ExpectedConditions.urlContains("/secure"));
-
-            String flashText = wait
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")))
-                    .getText();
-
-            System.out.println(flashText);
-
-            Assert.assertTrue(flashText.contains("You logged into a secure area!"));
-
-        } finally {
-            driver.quit();
-        }
+        System.out.println(flashText);
+        Assert.assertTrue(flashText.contains("You logged into a secure area!"));
     }
 }
